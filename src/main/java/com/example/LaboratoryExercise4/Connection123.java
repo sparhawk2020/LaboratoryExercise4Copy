b318a023a1fde87463b94a0f5adf683ecb13c4ce
sparhawk2020 <cjjc20001@yahoo.com>
2021-07-11 21:36:04 -0700

    First Version

--- src/main/java/com/example/LaboratoryExercise4/Connection123.java
+++ src/main/java/com/example/LaboratoryExercise4/Connection123.java
@@ -10,7 +10,13 @@ public class Connection123 {
 
     public java.sql.Connection connect() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
-        java.sql.Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost/inventory", "root", "");
+
+
+
+       // java.sql.Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost/inventory", "root", "");
+
+
+        java.sql.Connection con1 = DriverManager.getConnection("  mysql://b736d43c1d799b:c8af87e1@us-cdbr-east-04.cleardb.com/heroku_239361c74896bd2?reconnect=true", "b736d43c1d799b", "c8af87e1");
 
         return con1;
 
